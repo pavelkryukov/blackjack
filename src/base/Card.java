ee723fb0597ef1d5a06bb154c1000754716fc7f7
unknown <Paul@.(none)>
2013-02-23 23:06:44 +0400

    Pavel Kryukov's first ever GIT commit

--- src/base/Card.java
+++ src/base/Card.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 Svyatoslav Kuzmich. All rights reserved.
+ * Copyright (c) 2013 Svyatoslav Kuzmich, Pavel Kryukov. All rights reserved.
  */
 package base;
 
@@ -9,20 +9,21 @@ package base;
 public class Card {
     public enum Rank  { ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
                         EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE };
-    
+
     public enum Suit  { SPADES, HEARTS, DIAMONDS, CLUBS };
-    
+
     public Card(Rank rank, Suit suit) {
         this.rank = rank;
         this.suit = suit;
     }
-    
+
     private Rank rank;
     private Suit suit;
-    
+
     public Rank getRank() { return rank; }
     public void setRank(Rank rank) { this.rank = rank; }
-    
+
     public Suit getSuit() { return suit; }
     public void setSuit(Suit suit) { this.suit = suit; }
 }
+
